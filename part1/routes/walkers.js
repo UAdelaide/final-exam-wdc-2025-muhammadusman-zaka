1434c762be3cd43def8214c6cc4687154c4da8de
WDC Student (missing default git user name) <usman.zaka04@gmail.com>
2025-06-20 05:38:59 +0000

    [automatic commit] 2025-06-20 05:38:59 UTC 103.29.173.247

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -11,8 +11,8 @@ router.get('/summary', async (req, res) => {
                     AVG(wr.rating) AS average_rating,
                     SUM(CASE WHEN wreq.status = 'completed' THEN 1 ELSE 0 END) AS completed_walks
             FROM Users u
-            LEFT JOIN WalkRatings r ON u.user_id = r.walker_id
-            LEFT JOIN WalkRequests wr ON wr.request_id = r.request_id
+            LEFT JOIN WalkRatings wr ON u.user_id = wr.walker_id
+            LEFT JOIN WalkRequests wr ON wr.request_id = wr.request_id
             WHERE u.role = 'walker'
             GROUP BY u.username`
 
