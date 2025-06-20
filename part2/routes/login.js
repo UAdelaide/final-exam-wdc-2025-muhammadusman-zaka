b25cfa4a1f3178e720199727d7ec386da0aa2755
WDC Student (missing default git user name) <usman.zaka04@gmail.com>
2025-06-20 08:08:50 +0000

    [automatic commit] 2025-06-20 08:08:50 UTC 103.29.173.247

--- part2/routes/login.js
+++ part2/routes/login.js
@@ -4,5 +4,9 @@ var dbConnection = require('../db');
 
 router.post('/', async(req, res) => {
     const { username, password } = req.body;
+    try{
+        var connection = await dbConnection();
+        
+    }
 
 });
