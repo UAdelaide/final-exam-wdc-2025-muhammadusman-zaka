a457310eb137d9a1a8f2572e8ee0704d2c059b25
WDC Student (missing default git user name) <usman.zaka04@gmail.com>
2025-06-20 05:00:11 +0000

    [automatic commit] 2025-06-20 05:00:10 UTC 103.29.173.247

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -6,7 +6,9 @@ router.get('/', async(req, res) => {
     try{
         var connection = await dbConnection();
         var [rows] = await connection.query(
-            `SELECT d.name AS dog_name, d.size, u.username AS owner_username`
+            `SELECT d.name AS dog_name, d.size, u.username AS owner_username
+             FROM Dogs d
+             Join Users u on d.owner_name`
         );
     }
 });
