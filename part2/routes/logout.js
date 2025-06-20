bc0dd97f2a2e88d4f1fbc71d45b6ddf978c66b94
WDC Student (missing default git user name) <usman.zaka04@gmail.com>
2025-06-20 11:24:18 +0000

    [automatic commit] 2025-06-20 11:24:18 UTC

--- part2/routes/logout.js
+++ part2/routes/logout.js
@@ -4,7 +4,7 @@ var router = express.Router();
 router.post('/', (req,res) => {
     req.session.destroy((err) => {
         if(err) return res.status(500).json({ error: 'Failed to logout' });
-        res.status(200).json({ message: ''})
+        res.status(200).json({ message: 'Logged out succesfully' });
     });
 
 });
