3f55150924f79eb2284c6ee81c0f818fcd73a51d
WDC Student (missing default git user name) <usman.zaka04@gmail.com>
2025-06-20 08:14:26 +0000

    [automatic commit] 2025-06-20 08:14:26 UTC ;; communications error to 208.67.222.222#53: timed out 103.29.173.247

--- part2/routes/login.js
+++ part2/routes/login.js
@@ -24,7 +24,7 @@ router.post('/', async(req, res) => {
             return res.status(401).json({ error: 'Invalid username or password'});
         }
 
-        res.status(200).json({ username: user.username, role: user.role});
+        return res.status(200).json({ username: user.username, role: user.role});
     } catch(err){
         res.status(500).json({ error: 'Server error' });
     }
