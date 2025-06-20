6737812c6f4096ce6266ffb81a126a907e03920a
WDC Student (missing default git user name) <usman.zaka04@gmail.com>
2025-06-20 12:04:33 +0000

    [automatic commit] 2025-06-20 12:04:33 UTC

--- part2/app.js
+++ part2/app.js
@@ -28,7 +28,6 @@ const { log } = require('console');
 app.use('/api/login', loginRouter);
 
 app.use('/api/walks', walkRoutes);
-app.use('/api/users', userRoutes);
 
 app.use('/api/dogs', dogRouter);
 
