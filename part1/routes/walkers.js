7a1bc8dff583ffcf24f258dbd3acbd1094486de8
WDC Student (missing default git user name) <usman.zaka04@gmail.com>
2025-06-20 06:07:41 +0000

    [automatic commit] 2025-06-20 06:07:40 UTC 103.29.173.247

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -4,7 +4,7 @@ var dbConnection = require('../db');
 
 router.get('/summary', async (req, res) => {
     try{
-        var connection = dbConnection();
+        var connection = await dbConnection();
         const [rows] = await connection.query(
             `SELECT u.username AS walker_username,
                     COUNT(wr.rating) AS total_ratings,
