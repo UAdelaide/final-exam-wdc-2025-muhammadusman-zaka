54957abc03532686e513ad8227d1767c1c39b411
WDC Student (missing default git user name) <usman.zaka04@gmail.com>
2025-06-20 11:18:25 +0000

    [automatic commit] 2025-06-20 11:18:25 UTC

--- part2/app.js
+++ part2/app.js
@@ -34,7 +34,7 @@ app.use('/api/dogs', dogRouter);
 
 app.use('/api/users', usRoute);
 
-app.use('/logout',logoutRouter);
+app.use('/logout', logoutRouter);
 
 // Export the app instead of listening here
 module.exports = app;
