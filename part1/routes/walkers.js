ce5490b235163812ff0343958d3085a6f10f57f9
WDC Student (missing default git user name) <usman.zaka04@gmail.com>
2025-06-20 05:25:54 +0000

    [automatic commit] 2025-06-20 05:25:54 UTC 103.29.173.247

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -9,7 +9,7 @@ router.get('/summary', async (req, res) => {
             `SELECT u.username AS walker_username,
                     COUNT(wr.rating) AS total_ratings,
                     AVG(wr.rating) AS average_rating,
-                    SUM(CASE WHEN wreq.status = 'completed' THEN 1)`
+                    SUM(CASE WHEN wreq.status = 'completed' THEN 1 ELSE 0 END) AS completed_walks`
 
         );
     }
