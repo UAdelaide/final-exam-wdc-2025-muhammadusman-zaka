e2752297dd05b268e056eae8731a3df69f6621f4
WDC Student (missing default git user name) <usman.zaka04@gmail.com>
2025-06-20 12:03:32 +0000

    [automatic commit] 2025-06-20 12:03:32 UTC

--- part2/routes/users.js
+++ part2/routes/users.js
@@ -14,4 +14,37 @@ router.get('/me', (req, res) => {
     });
 });
 
+router.post('/login', async(req, res) => {
+    const { username, password } = req.body;
+    try{
+        var connection = await dbConnection();
+        var [rows] = await connection.query(
+            `SELECT user_id, username, role, password_hash
+             FROM Users
+             WHERE username = ? LIMIT 1`,
+             [username]
+        );
+
+        await connection.end();
+
+        // The user does not exist in the database
+        if(rows.length === 0){
+            return res.status(401).json({ error: 'Invalid username or password'});
+        }
+
+        // Passwords do not match
+        var user = rows[0];
+        if(user.password_hash !== password){
+            return res.status(401).json({ error: 'Invalid username or password'});
+        }
+        req.session.user_id = user.user_id;
+        req.session.username = user.username;
+        req.session.role = user.role;
+
+        return res.status(200).json({ username: user.username, role: user.role});
+    } catch(err){
+        res.status(500).json({ error: 'Server error' });
+    }
+});
+
 module.exports = router;
