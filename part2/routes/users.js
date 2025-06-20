ac6e742fedbae5cd74f3b4adc3d42b27c9b69c84
WDC Student (missing default git user name) <usman.zaka04@gmail.com>
2025-06-20 12:06:16 +0000

    [automatic commit] 2025-06-20 12:06:16 UTC

--- part2/routes/users.js
+++ part2/routes/users.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/me', (req, res) => {
-    console.log('Current session:', req.session);
     if(!req.session || !req.session.user_id){
         return res.status(401).json({ error: 'Not authenticated' });
     }
